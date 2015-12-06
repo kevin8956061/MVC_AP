c7477a10a678a3d4b49a65afb6f849faf821d6ba
kevin8956061 <kevin8956061@gmail.com>
2015-12-06 23:16:08 +0800

    首次將專案加入Git本地儲存庫

---
+++ MVC_AP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_AP.MvcApplication" Language="C#" %>
---
+++ MVC_AP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_AP.Startup))]
+namespace MVC_AP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
